d8ff2f820ca0a97559ea7f0b7cbbb1cc8ba93ddb
Cmrm <fis.carlosmanuel@gmail.com>
2015-05-17 01:39:05 -0500

    Soporte preliminar de AC de varios carriles.

--- FreewayAC/CellularAutomata.cpp
+++ FreewayAC/CellularAutomata.cpp
@@ -25,6 +25,13 @@ void aux_progress_bar(double progress)
         std::cout << std::endl;
 }
 
+////////////////////////////////////
+//                                //
+//        Autómatas celulares     //
+//        de carril único.        //
+//                                //
+////////////////////////////////////
+
 
 /****************************
 *                           *
@@ -431,4 +438,365 @@ void delete_ca()
     openca = NULL;
     smartca = NULL;
     cellularautomata = NULL;
+}
+
+
+////////////////////////////////////
+//                                //
+//      Autómatas celulares       //
+//      de varios carriles.       //
+//                                //
+////////////////////////////////////
+
+/****************************
+*                           *
+*       AC Básico ML        *
+*                           *
+****************************/
+
+CAElement::CAElement(const int lanes, const int def_val)
+{
+	m_lane.assign(lanes, def_val);
+}
+int &CAElement::operator[](const unsigned &iter)
+{
+	return m_lane[iter];
+}
+
+CellularAutomataML::CellularAutomataML(const unsigned &size, const unsigned &lanes, const double &density, 
+	                                   const int &vmax, const double &rand_prob)
+{
+    // Inicializa variables.
+    m_size = size;
+	m_lanes = lanes;
+    m_vmax = vmax;
+    m_rand_prob = rand_prob;
+
+    m_ca.assign(size, CAElement(lanes));
+    m_ca_temp.assign(size, CAElement(lanes));
+    m_ca_history.clear();
+    m_ca_flow_history.clear();
+    unsigned vehicles = (unsigned)(((double)size)*density);
+
+    // Asigna semilla a numeros aleatorios.
+    unsigned seed = static_cast<unsigned int>(time(NULL));
+    m_drand.seed(seed);
+    m_irand.seed(seed);
+
+    // Coloca autos al azar.
+    for (unsigned i=0; i<vehicles; ++i)
+    {
+        unsigned pos, lane;
+        do
+        {
+            pos = m_irand() % m_ca.size();
+			lane = m_irand() % 2;
+        }
+        while (m_ca[pos][lane] != -1);
+        m_ca[pos][lane] = 1;
+    }
+    m_ca_history.push_back(m_ca);
+}
+void CellularAutomataML::Print()
+{
+    // Imprime valores del automata celular en la terminal.
+	for (unsigned i=0; i<m_lanes; ++i)
+	{
+		for (unsigned j=0; j<m_ca.size(); ++j)
+		{
+			if (m_ca[j][i] == -1)
+				cout << "-";
+			else
+				cout << m_ca[j][i];
+		}
+		cout << endl;
+	}
+	for (unsigned i=0; i<m_ca.size(); ++i)
+		cout << ".";
+	cout << endl;
+}
+void CellularAutomataML::DrawHistory()
+{
+    unsigned bmp_height;
+	if (m_lanes == 1)
+		bmp_height =  m_ca_history.size();
+	else
+		bmp_height =  m_lanes*m_ca_history.size() + m_lanes;
+
+	unsigned height =  m_ca_history.size();
+    unsigned width = m_size;
+    BMPWriter writer("ca.bmp", width, bmp_height);
+    BMPPixel* bmpData = new BMPPixel[width];
+	BMPPixel color;
+
+    for (int i=height-1; i>=0; i--)
+    {
+		for (unsigned l=0; l<m_lanes; ++l)
+		{
+			for (unsigned j=0; j<width; ++j)
+			{
+				if (m_ca_history[i][j][l] == -1)
+					color = BMPPixel((char)255, (char)255, (char)255);
+				else
+					color = BMPPixel(0, 0, (char)(255.0*(double)m_ca_history[i][j][l]/(double)m_vmax));
+				bmpData[j] = color;
+			}
+			writer.WriteLine(bmpData);
+		}
+
+		for (unsigned j=0; j<width; ++j)
+		{
+			color = BMPPixel((char)0, (char)255, (char)0);
+			bmpData[j] = color;
+		}
+		writer.WriteLine(bmpData);
+    }
+    writer.CloseBMP();
+    delete[] bmpData;
+}
+void CellularAutomataML::DrawFlowHistory()
+{
+    unsigned bmp_height;
+	if (m_lanes == 1)
+		bmp_height =  m_ca_flow_history.size();
+	else
+		bmp_height =  m_lanes*m_ca_flow_history.size() + m_lanes;
+
+	unsigned height =  m_ca_flow_history.size();
+    unsigned width = m_size;
+    BMPWriter writer("ca_flow.bmp", width, bmp_height);
+    BMPPixel* bmpData = new BMPPixel[width];
+	BMPPixel color;
+
+    for (int i=height-1; i>=0; i--)
+    {
+		for (unsigned l=0; l<m_lanes; ++l)
+		{
+			for (unsigned j=0; j<width; ++j)
+			{
+				if (m_ca_flow_history[i][j][l] == -1)
+					color = BMPPixel((char)255, (char)255, (char)255);
+				else
+					color = BMPPixel(0, 0, (char)(255.0*(double)m_ca_flow_history[i][j][l]/(double)m_vmax));
+				bmpData[j] = color;
+			}
+			writer.WriteLine(bmpData);
+		}
+
+		for (unsigned j=0; j<width; ++j)
+		{
+			color = BMPPixel((char)0, (char)255, (char)0);
+			bmpData[j] = color;
+		}
+		writer.WriteLine(bmpData);
+    }
+    writer.CloseBMP();
+    delete[] bmpData;
+}
+void CellularAutomataML::Step()
+{
+	// Primero hace los cambios de carril.
+	if (m_lanes > 1)
+	{
+		for (unsigned i=0; i<m_ca.size(); ++i)
+		{
+			for (unsigned j=0; j<m_lanes; ++j)
+			{
+				// Si no puede acelerar
+				if (!((At(i, j) <= m_vmax) && (NextCarDist(i, j) > (At(i, j) + 1))))
+				{
+					if (NextCarDist(i, j) <= At(i, j))
+					{
+						// Intenta cambiar de carril.
+						bool left = false, right = false;
+						for (int k=(int)j-1; k<=(int)j+1 && k<(int)m_lanes; k++)
+						{
+							if (k<0 || k==j || At(i, k) != -1) continue;
+
+							// Busca auto anterior y verifica si puede cambiarse sin chocar.
+							int v, s = 0;
+							for (int l=i-1; s<=m_vmax; ++s, l--)
+							{
+								v = At(l, k);
+								if ((v != -1) && (v-s < 0))
+								{
+									// Ahora busca si en este nuevo carril puede acelerar.
+									if ((At(i, k) <= m_vmax) && (NextCarDist(i, k) > (At(i, k) + 1)))
+									{
+										// Marca carril como disponible.
+										if (k == j-1) left = true;
+										else right = true;
+									}
+									break;
+								}
+							}
+							if (s == m_vmax)
+							{
+								if ((At(i, k) <= m_vmax) && (NextCarDist(i, k) > (At(i, k) + 1)))
+								{
+									// Marca carril como disponible.
+									if (k == j-1) left = true;
+									else right = true;
+								}
+							}
+						}
+
+						// Si el carril está disponible adelenta.
+						if (left || right)
+						{
+							// Si ambos están disponibles apaga uno al azar.
+							if (left && right)
+							{
+								int select = m_irand() % 2;
+								if (select == 1) right = false;
+								else left = false;
+							}
+							unsigned new_lane;
+							if (left)
+								new_lane = j-1;
+							else
+								new_lane = j+1;
+
+							At(i, new_lane) = At(i, j);
+							At(i, j) = -1;
+						}
+					}
+				}
+			}
+		}
+	}
+
+    // Iterar sobre AC.
+    for (unsigned i=0; i<m_ca.size(); ++i)
+    {
+		for (unsigned j=0; j<m_lanes; ++j)
+		{
+			// Encuentra vehículo.
+			if (At(i, j) != -1)
+			{
+				// Aceleracion.
+				if ((At(i, j) <= m_vmax) && (NextCarDist(i, j) > (At(i, j) + 1)))
+					At(i, j)++;
+				else
+				{
+					// Frenado.
+					if (At(i, j) > 0)
+					{
+						int nd = NextCarDist(i, j);
+						if (nd <= m_ca[i][j])
+							At(i, j) = nd - 1;
+					}
+				}
+
+				// Aleatoriedad.
+				if ((At(i, j) > 0) && (m_drand() <= m_rand_prob))
+					At(i, j)--;
+			}
+		}
+    }
+
+    // Aplicar cambios.
+	Move();
+    m_ca_history.push_back(m_ca);
+}
+void CellularAutomataML::Evolve(const unsigned &iter, const bool &print)
+{
+	// Escribe condiciones iniciales.
+	if (print)
+		Print();
+
+    for (unsigned i=0; i<iter; ++i)
+	{
+        Step();
+		if (print)
+			Print();
+	}
+}
+unsigned CellularAutomataML::GetSize()
+{
+    return m_size;
+}
+unsigned CellularAutomataML::GetHistorySize()
+{
+    return m_ca_history.size();
+}
+int &CellularAutomataML::At(const int &i, const unsigned &lane, const CAS &ca)
+{
+    return At(i, lane, 0, ca);
+}
+
+
+/****************************
+*                           *
+*      AC Circular ML       *
+*                           *
+****************************/
+
+CircularCAML::CircularCAML(const unsigned size, const unsigned lanes, const double density, 
+	                       const int vmax, const double rand_prob) 
+                         : CellularAutomataML(size, lanes, density, vmax, rand_prob) {}
+int &CircularCAML::At(const int &i, const unsigned &lane, const unsigned &j, const CAS &ca)
+{
+    // Devuelve elemento de AC suponiendo una pista ciclica.
+	unsigned int pos;
+	if (i<0)
+	{
+		pos = abs(i) % m_ca.size();
+		pos = m_ca.size() - pos;
+	}
+	else
+		pos = i % m_ca.size();
+
+    switch (ca)
+    {
+    case CA_TEMP:
+        return m_ca_temp[pos][lane];
+        break;
+    case CA_FLOW_TEMP:
+        return m_ca_flow_temp[pos][lane];
+        break;
+    case CA_HISTORY:
+        return m_ca_history[j][pos][lane];
+        break;
+    case CA_FLOW_HISTORY:
+        return m_ca_flow_history[j][pos][lane];
+        break;
+    default:
+        return m_ca[pos][lane];
+        break;
+    };
+}
+int CircularCAML::NextCarDist(const int &pos, const unsigned &lane)
+{
+    unsigned dist = 1;
+    while (At(pos+dist, lane) == -1)
+        dist++;
+    return dist;
+}
+void CircularCAML::Move()
+{
+    m_ca_flow_temp.assign(m_size, CAElement(m_lanes, 0));
+    for (unsigned i=0; i<m_ca_temp.size(); ++i)
+	{
+		for (unsigned j=0; j<m_lanes; ++j)
+			m_ca_temp[i][j] = -1;
+	}
+
+    for (unsigned i=0; i<m_ca.size(); ++i)
+    {
+		for (unsigned j=0; j<m_lanes; ++j)
+		{
+			if (m_ca[i][j] != -1)
+			{
+				// Cambia las posiciones de los autos en AC.
+				At(i+m_ca[i][j], j, CA_TEMP) = m_ca[i][j];
+
+				// Marca las casillas donde hay flujo de autos.
+				for (unsigned k=i; k<i+m_ca[i][j]; ++k)
+					At(k, j, CA_FLOW_TEMP) = 1;
+			}
+		}
+    }
+    m_ca_flow_history.push_back(m_ca_flow_temp);
+    m_ca.assign(m_ca_temp.begin(), m_ca_temp.end());
 }
--- FreewayAC/CellularAutomata.h
+++ FreewayAC/CellularAutomata.h
@@ -40,6 +40,13 @@ template <class N> N aux_string_to_num(const std::string& s)
     return static_cast<N>(x);
 }
 
+////////////////////////////////////
+//                                //
+//        Autómatas celulares     //
+//        de carril único.        //
+//                                //
+////////////////////////////////////
+
 /****************************
 *                           *
 *         AC Básico         *
@@ -142,3 +149,71 @@ public:
 
 CellularAutomata* create_ca(CA_TYPE ct, const unsigned &size, const double &density, const int &vmax, const double &rand_prob, const double &extra = 0.0);
 void delete_ca();
+
+
+
+////////////////////////////////////
+//                                //
+//      Autómatas celulares       //
+//      de varios carriles.       //
+//                                //
+////////////////////////////////////
+
+class CAElement
+{
+	std::vector<int> m_lane;
+public:
+	CAElement(const int lanes, const int def_val = -1);
+	int &operator[](const unsigned &iter);
+};
+
+/****************************
+*                           *
+*   AC Básico Multicarril   *
+*                           *
+****************************/
+
+class CellularAutomataML
+{
+protected:
+    int m_vmax;
+	unsigned m_lanes;
+    double m_rand_prob;
+    unsigned m_size;
+    std::vector<CAElement> m_ca, m_ca_temp, m_ca_flow_temp;    // Automata celular. -1 para casillas sin auto, y valores >= 0 indican velocidad del auto en esa casilla.
+    std::vector< std::vector<CAElement> > m_ca_history, m_ca_flow_history;
+    MTRand m_drand;            // Generador de aleatorios (flotantes) entre 0 y 1.
+    MTRand_int32 m_irand;      // Generador de enteros aleatorios.
+
+public:
+    CellularAutomataML(const unsigned &size, const unsigned &lanes, const double &density, 
+		               const int &vmax, const double &rand_prob);
+    void Print();
+    void DrawHistory();
+    void DrawFlowHistory();
+    void Evolve(const unsigned &iter, const bool &print = false);
+    unsigned GetSize();
+    unsigned GetHistorySize();
+    int &At(const int &i, const unsigned &lane, const CAS &ca = CA);
+    virtual void Step();
+    virtual int &At(const int &i, const unsigned &lane, const unsigned &j, const CAS &ca) = 0;
+    virtual int NextCarDist(const int &pos, const unsigned &lane) = 0;
+    virtual void Move() = 0;
+};
+
+/****************************
+*                           *
+*      AC Circular ML       *
+*                           *
+****************************/
+
+class CircularCAML : public CellularAutomataML
+{
+public:
+    CircularCAML(const unsigned size, const unsigned int lanes, const double density, 
+		         const int vmax, const double rand_prob);
+    using CellularAutomataML::At;
+    int &At(const int &i, const unsigned &lane, const unsigned &j, const CAS &ca);
+    int NextCarDist(const int &pos, const unsigned &lane);
+    virtual void Move();
+};
